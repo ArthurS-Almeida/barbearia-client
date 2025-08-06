4797873be90ca49e50c58310b9f8e2636c079e75
dev-arthursantos <acad.arthur.alm@gmail.com>
2025-08-06 20:11:10 -0300

    Seça

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './styles/App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <div>Agendamento</div>
+        <div>Nome</div>
+        <div>Sair</div>
+      </header>
+      <div>
+        <div>Marcar</div>
+        <div>Cancelar</div>
+        <div>Alterar</div>
+      </div>
+      <div>
+
+
+      </div>
     </div>
   );
 }
