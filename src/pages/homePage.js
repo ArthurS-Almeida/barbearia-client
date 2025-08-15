6ce25fb19444624d303e473b951ca7e1a9e9c822
dev-arthursantos <acad.arthur.alm@gmail.com>
2025-08-15 12:10:23 -0300

    Correção em produção da estilização

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,27 +5,48 @@ import "../styles/homePage.css"
 
 function HomePage() {
 
-    
+
 
     // Mensagem caso não tenha agendamentos
     const msgEmpty = 'Não há agendamentos no momento...'
 
     // Ativando opção de Add
-    const [AddClient,setAddClient] = useState(null)
-    
-    const abrirAdd = () =>{
-        if (AddClient == null){
-            setAddClient(Add)
-        }
+    const [AddClient, setAddClient] = useState(null);
+
+    const abrirAdd = () => {
+        if (AddClient == null) {
+            setAddClient(Add);
+        } 
     }
+
+    // Array para mostrar agendamentos marcados
+    const listAppointments = [];
     return (
         <div id="containerHomePage">
-            <div id="homePage">
+
+            <header>
+                <span>Barbearia Premium</span>
+                <div>
+                    <span>Admin</span>{/* RECEBER DADOS SOBRE NOME*/}
+                </div>
+            </header>
+
+            <main id="homePage">
                 
-               
-            </div>
-                <button id="button-add" onClick={abrirAdd}>Adicionar</button>
+                <div id="container-agendados">
+
+                    
+
+                </div>
+                <div>
+                    <button id="button-add">Adicionar</button>
+                    <button> Inativar data</button>
+                </div>
                 {AddClient}
+            </main>
+            <footer>
+                <p>DESENVOLVIDO POR ARTHUR2SANTOS</p>
+            </footer>
         </div>
     );
 }
